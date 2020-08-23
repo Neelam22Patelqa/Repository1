24da94774c668bed3beca2776b3bbbba89003307
Neelam22Patel <neelam.uvbm@gmail.com>
2020-08-23 12:27:40 -0400

    i share and commit

---
+++ rr1/src/rr1/testcase1.java
@@ -0,0 +1,10 @@
+package rr1;
+
+public class testcase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is mastser master error");
+	}
+
+}
