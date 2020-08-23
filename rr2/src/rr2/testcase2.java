0e6ff36f59309b4c6275e2037f96e2865cc455b4
Neelam22Patel <neelam.uvbm@gmail.com>
2020-08-23 12:34:30 -0400

    lets see

---
+++ rr2/src/rr2/testcase2.java
@@ -0,0 +1,10 @@
+package rr2;
+
+public class testcase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("seond java progam trying to put in same clone remote repository");
+	}
+
+}
