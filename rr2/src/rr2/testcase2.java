43a6eddd6ce81cdef4afe90869cb87d433027d84
Neelam22Patel <neelam.uvbm@gmail.com>
2020-08-24 10:36:28 -0400

    correct spelling

--- rr2/src/rr2/testcase2.java
+++ rr2/src/rr2/testcase2.java
@@ -4,7 +4,7 @@ public class testcase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("seond java progam trying to put in same clone remote repository");
+		System.out.println("second java progam trying to put in same clone remote repository");
 	}
 
 }
